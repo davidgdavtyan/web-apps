ec5e1eb7336dd0c08ca81cabd69b2aadd0ad1d66
David Davtyan <david.davtyan@kellogg.northwestern.edu>
2024-02-23 08:26:02 +0000

    Hour 2

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,7 +1,8 @@
 class TacosController < ApplicationController
 
   def index
-    render :inline => "<h1>Tacos!!!</h1>"
+    #render :inline => "<h1>Tacos!!!</h1>"
+    render :template => 'tacos/index'
   end
 
 end
